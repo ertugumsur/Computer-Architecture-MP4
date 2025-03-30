6ec4738dd83e939bdccec270753d0c008f477897
Ishan Porwal <ishanporwaltennis@gmail.com>
2025-03-30 14:58:05 -0400

    added in program counter

--- program_counter.sv
+++ program_counter.sv
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-// pc.sv
+// Program Counter
 //
 // The Program Counter (PC) stores the address of the current instruction.
 // Each cycle, the top-level module uses this address to fetch the instruction
@@ -7,3 +7,22 @@
 // the PC is updated — usually to PC + 4, or to a new address if the instruction
 // is a branch or jump — so that it points to the next instruction to execute.
 //-----------------------------------------------------------------------------
+module program_counter (
+    input logic clk,
+    input logic reset,
+    input logic select,
+    input logic [31:0] immediate,       // to store the immediate for jumps/branches
+    output logic [31:0] pc              // 32-bit program counter
+);
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            pc <= 0;
+        end
+        else if (select) begin
+            pc <= pc + immediate;
+        end
+        else begin
+            pc <= pc + 4;
+        end
+    end
+endmodule
