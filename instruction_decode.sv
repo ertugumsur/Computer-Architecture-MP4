a4671cab76940972c5e5ed3e350ae6ec81f60392
Ahan Trivedi <atrivedi@olin.edu>
2025-04-03 13:25:25 -0400

    Finished instruction_decode.sv

--- instruction_decode.sv
+++ instruction_decode.sv
@@ -8,6 +8,26 @@
 //
 // Used In: Decode
 //
-// File Contributor(s):
+// File Contributor(s): Ahan Trivedi
 //-----------------------------------------------------------------------------
 
+// Given a 32 bit instruction we want... [31:25] funct7 | [24:20] rs2 | [19:15] rs1 | [14:12] funct3 | [11:7] rd | [6:0] opcode
+
+module instruction_decode(
+    input logic [31:0] instruction, // Raw 32 bit instruction pulled from memory
+    output logic [6:0] opcode, // Operation Code
+    output logic [4:0] rd, // Destination Register
+    output logic [2:0] funct3, // Function 3 bits
+    output logic [4:0] rs1, // Source Register 1
+    output logic [4:0] rs2, // Source Register 2
+    output logic [6:0] funct7 // Function 7 bits
+);
+
+    assign opcode = instruction[6:0];
+    assign rd     = instruction[11:7];
+    assign funct3 = instruction[14:12];
+    assign rs1    = instruction[19:15];
+    assign rs2    = instruction[24:20];
+    assign funct7 = instruction[31:25];
+
+endmodule
